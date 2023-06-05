08a0660f11519360cab2f5ed7575d2f4bc06fde1
Jean-rené Decoster <jeanrene.decoster@gmail.com>
2023-06-05 10:45:11 +0200

    first hour done

--- index.js
+++ index.js
@@ -20,3 +20,42 @@
 // h1.textContent = "this is imperative";
 // h1.className = "header";
 // document.getElementById("root").append(h1);
+
+// const element = <h1 className="header">This is JSX </h1>;
+// console.log(element);
+// ReactDOM.render(element, document.getElementById("root"));
+
+// const page = (
+//   <div>
+//     <h1 className="header">This is JSX</h1>
+//     <p>This is paragraph</p>
+//   </div>
+// );
+// console.log(page);
+// ReactDOM.render(page, document.getElementById("root"));
+
+// const NavBAr = (
+//   <nav>
+//     <h1>Yo Hello</h1>
+//     <ul>
+//       <li>Pricing</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   </nav>
+// );
+// ReactDOM.render(NavBAr, document.getElementById("root"));
+const page = (
+  <div>
+    <h1>My awesome website in react</h1>
+    <h3>here are the reason why i love react</h3>
+    <ol>
+      <li>Composable</li>
+      <li>Declarative</li>
+      <li>Hireable skill </li>
+      <li>Actively maintend by skilled root </li>
+    </ol>
+  </div>
+);
+document.getElementById("root").append(JSON.stringify(page));
+ReactDOM.render(page, document.getElementById("root"));
