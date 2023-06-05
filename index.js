10c21b58744ddca54ec12fc519236706de525628
Jean-rené Decoster <jeanrene.decoster@gmail.com>
2023-06-05 10:17:04 +0200

    first hour

---
+++ index.js
@@ -0,0 +1,22 @@
+// ReactDOM.render(<h1>Hello everyone</h1>, document.getElementById("root"));
+
+// function Main() {
+//   return <h3>Hello from led</h3>;
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <ul>
+//       <li>1</li>
+//       <li>2</li>
+//     </ul>
+//     <Main />
+//   </div>,
+
+//   document.getElementById("root")
+// );
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "this is imperative";
+// h1.className = "header";
+// document.getElementById("root").append(h1);
